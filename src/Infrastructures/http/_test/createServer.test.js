c3b7916f3924dbf36a10a67e92daaa25ec082d13
Muhammad Fakhryan <fakhryandev@gmail.com>
2022-09-01 22:16:10 +0700

    build: create server error handling

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -12,6 +12,17 @@ describe('HTTP server', () => {
     await UsersTableTestHelper.cleanTable()
   })
 
+  it('should response 404 when request unregistered route', async () => {
+    const server = await createServer({})
+
+    const response = await server.inject({
+      method: 'GET',
+      url: '/unregisteredRoute',
+    })
+
+    expect(response.statusCode).toEqual(404)
+  })
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
@@ -33,6 +44,153 @@ describe('HTTP server', () => {
       expect(response.statusCode).toEqual(201)
       expect(responseJson.status).toEqual('success')
       expect(responseJson.data.addedUser).toBeDefined()
+    })
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        fullname: 'Test User',
+        password: 'secret',
+      }
+      const server = await createServer(container)
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'
+      )
+    })
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'testuser',
+        password: 'secret',
+        fullname: ['Test User'],
+      }
+      const server = await createServer(container)
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena tipe data tidak sesuai'
+      )
+    })
+
+    it('should response 400 when username more than 50 character', async () => {
+      // Arrange
+      const requestPayload = {
+        username:
+          'testusertestusertestusertestusertestusertestusertestusertestusertestusertestusertestusertestuser',
+        password: 'secret',
+        fullname: 'Test User',
+      }
+      const server = await createServer(container)
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena karakter username melebihi batas limit'
+      )
+    })
+
+    it('should response 400 when username contain restricted character ', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'test user',
+        password: 'secret',
+        fullname: 'Test User',
+      }
+      const server = await createServer(container)
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat user baru karena username mengandung karakter terlarang'
+      )
+    })
+
+    it('should response 400 when username unavailable', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ username: 'testuser' })
+      const requestPayload = {
+        username: 'testuser',
+        password: 'super_secret',
+        fullname: 'Test User',
+      }
+      const server = await createServer(container)
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('username tidak tersedia')
+    })
+
+    it('should handle server error correctly', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'testuser',
+        password: 'super_secret',
+        fullname: 'Test User',
+      }
+      const server = await createServer({})
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      })
+
+      // Assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(500)
+      expect(responseJson.status).toEqual('error')
+      expect(responseJson.message).toEqual('terjadi kegagalan pada server kami')
     })
   })
 })
