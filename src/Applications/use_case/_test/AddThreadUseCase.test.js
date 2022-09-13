49a083254b501f0d5b9c754c9b62f3c072b09904
Muhammad Fakhryan <fakhryandev@gmail.com>
2022-09-13 20:59:56 +0700

    build: add add thread use case

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,36 @@
+const AddThreadUseCase = require('../AddThreadUseCase')
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository')
+const NewThread = require('../../../Domains/threads/entities/NewThread')
+
+describe('AddThreadUseCase', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    const useCasePayload = {
+      title: 'new thread title',
+      body: 'new thread body',
+      owner: 'user-123',
+    }
+
+    const expectedAddedThread = new AddThreadUseCase({
+      id: 'thread-123',
+      title: useCasePayload.title,
+      owner: useCasePayload.owner,
+    })
+
+    const mockThreadRepository = new ThreadRepository()
+
+    mockThreadRepository.addThread = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(expectedAddedThread))
+
+    const addThreadUseCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    })
+
+    const addedThread = await addThreadUseCase.execute(useCasePayload)
+
+    expect(addedThread).toStrictEqual(expectedAddedThread)
+    expect(mockThreadRepository.addThread).toBeCalledWith(
+      new NewThread(useCasePayload)
+    )
+  })
+})
