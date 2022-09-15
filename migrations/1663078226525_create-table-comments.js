2b4e5eef12cf3cd6df432844fea4d27e4c5dc88d
Muhammad Fakhryan <fakhryandev@gmail.com>
2022-09-15 21:06:32 +0700

    migrations: create comments table

---
+++ migrations/1663078226525_create-table-comments.js
@@ -0,0 +1,47 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      default: false,
+    },
+  })
+
+  pgm.createConstraint(
+    'comments',
+    'fk_comments.owner_id_users.id',
+    'FOREIGN KEY(owner_id) REFERENCES users(id) ON DELETE CASCADE'
+  )
+
+  pgm.createConstraint(
+    'comments',
+    'fk_comments.thread_id_threads.id',
+    'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE'
+  )
+}
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('comments', 'fk_comments.owner_id_users.id')
+  pgm.dropConstraint('comments', 'fk_comments.thread_id_threads.id')
+  pgm.dropTabe('comments')
+}
---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+
+const pool = require('../src/Infrastructures/database/postgres/pool')
+
+const CommentsTableTestHelper = {
+  async findCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    }
+
+    const { rows } = await pool.query(query)
+    return rows
+  },
+
+  async cleanTable() {
+    await pool.query('TRUNCATE TABLE comments')
+  },
+}
+
+module.exports = CommentsTableTestHelper
